f2fa77e845ee04dfceb4fe1deee2f5607389cb83
EmelineD <emeline.duquenne@etu.u-bordeaux.fr>
2015-08-03 17:14:29 +0200

    Avancée de la page HTML

---
+++ NewsCreator.js
@@ -0,0 +1,37 @@
+function get_value(){
+	var title=document.getElementById("title").value;
+	var intro=document.getElementById("intro").value;
+	var content=document.getElementById("content").value;
+	console.log(title,intro,content);
+}
+
+function initNews(){
+	var icons_name=[];
+	var icons_type=[];
+	for (var n in img){
+		icons_name.push(img[n].name);
+		icons_type.push(img[n].type)
+	}
+	for (var t in icons_name ){
+		if(icons_type[t]=="biology"){
+			var ident="biology";
+		}
+		else if(icons_type[t]=="teaching"){
+			var ident="teaching";
+		}
+		else{
+			var ident="misc";
+		}
+		document.getElementById(ident).innerHTML+='<input type="radio" name="' + icons_name[t] +'" value="' + t + '" id="' + t +'" /> <img src="../img/'+icons_name[t]+'_thumb.jpg">';
+	}
+
+	var date = new Date();
+	var day = date.getDate();
+	var month = date.getMonth()+1;
+	var year = date.getFullYear();
+
+	document.getElementById("pubdate").innerHTML+='<input type="number" min="2015" max="2030" value="'+year+'"/> <input type="number" min="1" max="12" value="'+month+'"/> <input type="number" min="1" max="31" value="'+day+'" />';
+}
+
+
+
